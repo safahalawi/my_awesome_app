08f99c13247f7923aea4bbab13c0417ac446e37f
Safa <Safa.m.halawi@gmail.com>
2021-03-03 21:59:17 +0200

    first bunch of exercises

---
+++ 1. first bunch of exercises/app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :role, inclusion: {in: ["member" , "admin"]}
+
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def full_name=(val)
+    pieces = val.split
+    self.first_name = pieces[0]
+    self.last_name = pieces[1]
+  end
+end
