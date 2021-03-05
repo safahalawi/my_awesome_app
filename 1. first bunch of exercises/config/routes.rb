dcb5a4c63f1f9b5f1aeb1b7561cf1e275c4181c9
Safa <Safa.m.halawi@gmail.com>
2021-03-05 10:48:25 +0200

    hello

--- 1. first bunch of exercises/config/routes.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do
-  resources :users
-  root 'login#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
--- db/migrate/20210301230035_create_users.rb
+++ db/migrate/20210301230035_create_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
@@ -5,7 +7,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :last_name
       t.string :email
       t.string :role
-      
+
       t.timestamps
     end
   end
